48b0735b75ba262df8af20b36be193a82e858f6c
Andre Kot <andrii.bekey@gmail.com>
2020-03-01 11:25:31 +0300

    Initial commit

---
+++ src/Courses/CoursesOne.java
@@ -0,0 +1,10 @@
+package Courses;
+
+public class CoursesOne {
+    public static void main(String[] args) {
+        int i =12;
+        if (i > 42) {
+            System.out.println("Hi");
+        }
+    }
+}
---
+++ src/Task1/Task1.java
@@ -0,0 +1,66 @@
+package Task1;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Scanner;
+import java.lang.*;
+
+public class Task1 {
+//Ввести целые числа как аргументы командной строки, подсчитать их суммы (произведения) и вывести результат на консоль
+//    public static void main(String[] args) {
+//        int value = Scanner.nextInt();
+//        int sum = 0;
+//        int com = 1;
+//        while (value != 0) {  //подсчитать сумму чисел
+//            sum = sum + value % 10;
+//            value = value / 10;
+//        }
+//        System.out.println("Cумма введённых чисел: " + sum);
+       /* while (value != 0) {   //подсчитать произведения чисел
+        com *= value % 10;
+        value = value / 10;
+    }
+        System.out.println("Произведение введённых чисел: " + com);*/
+
+
+//        int s=0, p=1;
+//        for(int i=0; i<args.length; i++)
+//        {
+//            int atata=Integer.parseInt(args[i]);
+//            s=s + atata;
+//            p=p * atata;
+//        }
+//        System.out.println("Sum = " + s);
+//        System.out.println("Product of numbers = "+ p);
+    }
+}
+
+
+/*
+//Приветствовать любого пользователя при вводе его имени через командную строку.
+    public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
+        System.out.println("Hello " + str.nextLine() + "!");
+    }
+//Отобразить в окне консоли аргументы командной строки в обратном порядке.
+    public static void main(String args[]){
+        StringBuffer strb = new StringBuffer("1 2 3 4 5 6 7 8 9 10");
+        System.out.println("String buffer  = " + strb);
+
+        // Тут Реверсит
+        strb.reverse();
+        System.out.println("String buffer after reversing = " + strb);
+    }
+//3. Вывести заданное количество случайных чисел с переходом и без перехода на новую строку
+    public static void main(String[] args) {
+        System.out.println("Enter your password and press Enter.");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        scan.close();
+        if (name.equals("12345")) {
+            System.out.println("Correct password, " + name);
+        } else {
+            System.out.println("The password is not correct");
+        }
+    }
+ */
